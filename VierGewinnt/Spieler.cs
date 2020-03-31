accdf7dc9e10c7e8628e5990321de405354f7a4e
jmbarry00 <21r5410@bztf.ch>
2020-03-31 07:28:03 +0200

    erstes grobes Model mit Attributen

---
+++ VierGewinnt/Spieler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VierGewinnt
+{
+    class Spieler
+    {
+        public bool player1 = false;
+        public bool player2 = false;
+        public string Name;
+
+        public Spieler(string name)
+        {
+            Name = name;
+        }
+
+        public string getNameP1()
+        {
+            return "";
+        }
+
+        public string getNameP2()
+        {
+            return "";
+        }
+
+        
+
+
+    }
+}
---
+++ VierGewinnt/Spielfeld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VierGewinnt
+{
+    class Spielfeld
+    {
+        int SpaltenX;
+        int ZeilenY;
+        
+        public Spielfeld(int x, int y)
+        {
+            SpaltenX = x;
+            ZeilenY = y;
+            int[,] feld = new int[SpaltenX, ZeilenY];
+            Spieler player = new Spieler("");
+            player.player1 = true;
+
+        }
+    }
+}
